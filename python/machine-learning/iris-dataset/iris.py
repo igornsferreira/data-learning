d4619b389813d6a6b155fa1481b428a016cf9b66
Igor Ferreira <igornsferreira@gmail.com>
2025-05-19 22:05:10 -0300

    iris dataset

--- python/machine-learning/iris-dataset/iris.py
+++ python/machine-learning/iris-dataset/iris.py
@@ -6,15 +6,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-# Dataset
 iris = load_iris()
 X = iris.data
 y = iris.target
 
-# Divisão entre treino e teste (70% treino, 30% teste)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Função para treinar e avaliar o Modelo
 def treinar_modelo(X_train, X_test, y_train, y_test, nome_modelo):
     modelo = DecisionTreeClassifier()
     modelo.fit(X_train, y_train)
